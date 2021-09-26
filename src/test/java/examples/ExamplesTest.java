c6a79fe212083d8edd18c9cd9677c3b7d43e83fd
Arunprasanth <prasantharun252@gmail.com>
2021-09-27 00:20:24 +0530

    Pushing project to Git

--- src/test/java/examples/ExamplesTest.java
+++ src/test/java/examples/ExamplesTest.java
@@ -4,7 +4,7 @@ import com.intuit.karate.junit5.Karate;
 
 class ExamplesTest {
     
-    // this will run all *.feature files that exist in sub-directories
+    // this will run all *.feature files that exist in sub-directories.
     // see https://github.com/intuit/karate#naming-conventions   
     @Karate.Test
     Karate testAll() {
